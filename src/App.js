f7119263d00f6e86bc5b79ca0639886fbb67bf75
Ollie Anderson <ollieaa17@gmail.com>
2021-02-03 20:22:06 +0000

    initial artist fetch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-
+import Artist from './components/Artist'
 import 'bulma'
 import './styles/style.scss'
 
 const App = () => {
+
+
   return <BrowserRouter>
     <Navbar />
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="project_2/" component={Home} />
+      <Route exact path="project_2/artist/:strArtist" component={Artist}/>
     </Switch>
     <Footer />
   </BrowserRouter>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,23 @@
+import axios from 'axios'
 import React, {Link} from 'react'
 
 
 export default function Home() {
 
+  const [artist, updateArtist] = useState('')
+
   return <section className="hero is-primary is-fullheight-with-navbar">
     <div className="hero-body">
       <div className='container'>
         <div>
-          <input className='input' type='text' placeholder='Artist/album name...' width='200'/>
+          <input className='input' type='text' placeholder='Artist/album name...' width='200' onChange={(event) => updateArtist(event.target.value)}/>
         </div>
         <div className='container has-text-centered'>
-          <button className='button'>Search</button>
+          <Link to={{
+            pathname: `project_2/artist/${artist}`
+            }}> 
+            <button className='button'>Search</button>
+          </Link>
         </div>
       </div>
     </div>
